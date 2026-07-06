15ced688a850067be8c0723c7be0fead8d2d8b17
Alok Kushwaha <alokkush051@gmail.com>
2026-07-06 22:45:48 +0530

    Added Razorpay payment integration

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { loginUser } from "../services/authService";
-import "./login.css";
+import "../assets/css/login.css";
 
 function Login() {
 
