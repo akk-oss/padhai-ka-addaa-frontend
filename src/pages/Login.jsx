0f12f25dc3fc7f43a69ca5362d2731f95ff4bb48
Alok Kushwaha <alokkush051@gmail.com>
2026-07-12 16:39:41 +0530

    Updated RegisterRequest and User and AuthService

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,134 +4,108 @@ import { loginUser } from "../services/authService";
 import "../assets/css/login.css";
 import { Link } from "react-router-dom";
 function Login() {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const handleLogin = async (e) => {
+    e.preventDefault();
 
-    const handleLogin = async (e) => {
+    try {
+      const response = await loginUser({
+        email,
+        password,
+      });
 
-        e.preventDefault();
+      console.log("Login Response:", response);
 
-        try {
+     localStorage.setItem("token", response.token);
+localStorage.setItem("role", response.role);
+localStorage.setItem("fullName", response.fullName);
+localStorage.setItem("email", response.email);
+localStorage.setItem("userId", response.id);
 
-            const response = await loginUser({
-                email,
-                password
-            });
+alert("Login Successful");
 
-            console.log("Login Response:", response);
+      if (response.role === "ADMIN") {
+        navigate("/admin/dashboard");
+      } else if (response.role === "TEACHER") {
+        navigate("/teacher/dashboard");
+      } else {
+        navigate("/student/dashboard");
+      }
+    } catch (error) {
+  console.log(error);
 
-            localStorage.setItem("token", response.token);
-            localStorage.setItem("role", response.role);
+  const message =
+    error.response?.data?.message ||
+    error.response?.data ||
+    "Invalid Email or Password";
 
-            alert("Login Successful");
+  alert(message);
+}
+  };
 
-            if (response.role === "ADMIN") {
+  return (
+    <div className="login_form_container">
+      <div className="login_form">
+        <form onSubmit={handleLogin}>
+          <h2>Login</h2>
 
-                navigate("/admin/dashboard");
+          <div className="input_group">
+            <i className="fa fa-user"></i>
 
-            } else if (response.role === "TEACHER") {
+            <input
+              type="email"
+              placeholder="Username"
+              className="input_text"
+              autoComplete="off"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
 
-                navigate("/teacher/dashboard");
+          <div className="input_group">
+            <i className="fa fa-unlock-alt"></i>
 
-            } else {
+            <input
+              type="password"
+              placeholder="Password"
+              className="input_text"
+              autoComplete="off"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
 
-                navigate("/student/dashboard");
+          <div className="button_group" id="login_button">
+            <input
+              type="submit"
+              value="Submit"
+              className="btn btn-success"
+              style={{
+                width: "300px",
+                height: "40px",
+                borderRadius: "50px",
+                fontSize: "20px",
+                color: "#00ccff52",
+                background: "none",
+              }}
+            />
+          </div>
 
-            }
+          <div className="fotter">
+            <a href="/forgot-password">Forgot Password ?</a>
 
-        } catch (error) {
-
-            console.log(error);
-
-            alert("Invalid Email or Password");
-
-        }
-
-    };
-
-    return (
-
-        <div className="login_form_container">
-
-            <div className="login_form">
-
-                <form onSubmit={handleLogin}>
-
-                    <h2>Login</h2>
-
-                    <div className="input_group">
-
-                        <i className="fa fa-user"></i>
-
-                        <input
-                            type="email"
-                            placeholder="Username"
-                            className="input_text"
-                            autoComplete="off"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            required
-                        />
-
-                    </div>
-
-                    <div className="input_group">
-
-                        <i className="fa fa-unlock-alt"></i>
-
-                        <input
-                            type="password"
-                            placeholder="Password"
-                            className="input_text"
-                            autoComplete="off"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            required
-                        />
-
-                    </div>
-
-                    <div className="button_group" id="login_button">
-
-                        <input
-                            type="submit"
-                            value="Submit"
-                            className="btn btn-success"
-                            style={{
-                                width: "300px",
-                                height: "40px",
-                                borderRadius: "50px",
-                                fontSize: "20px",
-                                color: "#00ccff52",
-                                background: "none"
-                            }}
-                        />
-
-                    </div>
-
-                    <div className="fotter">
-
-                        <a href="/forgot-password">
-                            Forgot Password ?
-                        </a>
-
-                       <Link to="/register">
-     Sign Up
-</Link>
-
-                    </div>
-
-                </form>
-
-            </div>
-
-        </div>
-
-    );
-
+            <Link to="/register">Sign Up</Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
